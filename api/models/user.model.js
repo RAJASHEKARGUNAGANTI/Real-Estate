4e80af95eb071409ce12854bc52a4a5f6d5779a0
Rajashekar Gunaganti <gunagantirajashekar@gmail.com>
2023-12-29 13:57:18 +0530

    create and add oauth components with google authentication

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,20 +1,24 @@
-import { timeStamp } from "console";
 import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema({
     username:{
-        type: 'string',
+        type: String,
         required: true,
         unique: true
     },
     email:{
-        type: 'string',
+        type: String,
         required: true,
         unique: true
     },
     password:{
-        type: 'string',
+        type: String,
         required: true
-    }
+    },
+    avatar:{
+        type: String,
+        default: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F10%2F05%2F22%2F37%2Fblank-profile-picture-973460_960_720.png&tbnid=GonJG9aLPTc5LM&vet=12ahUKEwj-6-SokrSDAxWkSGwGHX7bA9YQMygAegQIARBz..i&imgrefurl=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&docid=wg0CyFWNfK7o5M&w=720&h=720&q=profile&ved=2ahUKEwj-6-SokrSDAxWkSGwGHX7bA9YQMygAegQIARBz'
+    },
 },
 {
     timestamps: true
